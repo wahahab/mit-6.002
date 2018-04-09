ff60036dd86d37ba315651f18612f57459c4cba6
wahahab <wahahab11@gmail.com>
2018-04-09 09:18:07 +0800

    start ps2

---
+++ ps2/PS2_rectparity.py
@@ -0,0 +1,11 @@
+# template for 6.02 rectangular parity decoding using error triangulation
+import PS2_tests
+import numpy
+
+def rect_parity(codeword,nrows,ncols): 
+    ## YOUR CODE HERE
+    ## return the corrected data
+    return
+
+if __name__ == '__main__':
+    PS2_tests.test_correct_errors(rect_parity)
